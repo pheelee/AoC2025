b8d6cb2b585f1b9b0d806e76b18872c4bca0e5d7
m1ntax <77499818+m1ntax@users.noreply.github.com>
2025-12-04 12:45:22 +0100

    Add thoughts for number comparison logic
    Add initial comments explaining the logic for finding the highest number from a list.

---
+++ d3/d3.py
@@ -0,0 +1,3 @@
+#thoughts
+# iterate through the given numbers, starting at thefirst one and comparing to the next. if bigger and amount of numbers behind is big enough to create the highest number with the given amount of figures, take this one as the new biggest number. set startingindex for next round to this new biggest number.
+# repeat until you compared the last figure with the remaining given numbers
